75b4d9ea5c4dfde8543b9f660b6534299c04763f
Nathan Chancellor <nathan@kernel.org>
2021-07-12 20:35:53 -0700

    kernel: Update to Linux 5.14-rc1
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- kernel/build.sh
+++ kernel/build.sh
@@ -98,7 +98,7 @@ function setup_krnl_src() {
     if [[ -n ${KERNEL_SRC} ]]; then
         cd "${KERNEL_SRC}" || exit 1
     else
-        LINUX=linux-5.12
+        LINUX=linux-5.14-rc1
         LINUX_TARBALL=${KRNL}/${LINUX}.tar.gz
 
         # If we don't have the source tarball, download and verify it
